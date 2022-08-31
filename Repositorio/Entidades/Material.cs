f52237eff5e7a378df1cf50a45fa8346f4be96a1
GustavoPrim <gugahprm@gmail.com>
2022-08-31 20:47:20 -0300

    Corrigido a entidade Material

--- Repositorio/Entidades/Material.cs
+++ Repositorio/Entidades/Material.cs
@@ -2,6 +2,10 @@
 {
     public class Material
     {
-
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sku { get; set; }
+        public DateTime DataValidade { get; set; }
+        public string Descricao { get; set; }
     }
 }
