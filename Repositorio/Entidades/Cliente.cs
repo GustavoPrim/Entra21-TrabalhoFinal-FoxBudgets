a9894b8fc656df32f08dca2745f6ba9a4f5ad7c9
Guilherme DMueller <guilhermemuellerdzesigaleski@gmail.com>
2022-08-31 20:51:43 -0300

    Repositorio.Entidades => Criado parte de informações do cliente

---
+++ Repositorio/Entidades/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace Repositorio.Entidades
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string NomeConstrutora { get; set; }
+        public string Cpf { get; set; }
+        public string Cnpj { get; set; }
+
+
+    }
+}
