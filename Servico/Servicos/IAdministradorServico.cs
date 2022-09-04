22aa5a9936c3e974eb04c1d92f92610ce60eb5b3
Lucas Perez Alves <perezcfnpbnu@gmail.com>
2022-09-04 20:50:48 -0300

    Realizado o administradorServico e o ViewModels -Foi realizado o IAdministradorServico porem tem um erro a ser discutido com o professor nas entidades -foi acrescentado o SelectViewModel

--- Servico/Servicos/IAdministradorServico.cs
+++ Servico/Servicos/IAdministradorServico.cs
@@ -1,6 +1,14 @@
-﻿namespace Aplicacao.Administrador
+﻿using Servico.ViewModels.Administradores;
+
+namespace Aplicacao.Administrador
 {
     internal interface IAdministradorServico
     {
+        void Cadastrar(AdministradorCadastrarViewModel administradorCadastrarViewModel);
+        void Editar(AdministradorCadastrarViewModel administradorEditarViewModel);
+        void Apagar(int id);
+        Administrador ObterPorId(int id);
+        List<Administrador> ObterTodos();
+        IList<>
     }
 }
---
+++ Servico/ViewModels/SelectViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Servico.ViewModels
+{
+    internal class SelectViewModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
