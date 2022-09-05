781d985696b1ec1b2e76cca8ac0e5f92f8e475d4
Lucas Perez Alves <perezcfnpbnu@gmail.com>
2022-09-05 16:50:32 -0300

    Implementado o AdministradorEnum e os ViewModels -Foi criado a feature AdministradorEditarViewModel -Foi criado os ViewModels

---
+++ Servico/ViewModels/Administradores/AdministradorViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Repositorio.Enums;
+
+namespace Servico.ViewModels.Administradores
+{
+    internal class AdministradorViewModel
+    {
+        public int Id { get; set; }
+        public  AdministradorEnum { get; set; }
+    }
+}
