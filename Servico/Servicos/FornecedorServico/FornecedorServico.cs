89b0f3dda1608b112e0bda5b0cc8d636a1298d80
willljdev <willljdev@gmail.com>
2022-09-07 13:40:01 -0300

    Organizando arquivo fornecedor na pasta

--- Servico/Servicos/FornecedorServico/FornecedorServico.cs
+++ Servico/Servicos/FornecedorServico/FornecedorServico.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Servico
+namespace Servico.Servicos.FornecedorServico
 {
     internal class FornecedorServico
     {
