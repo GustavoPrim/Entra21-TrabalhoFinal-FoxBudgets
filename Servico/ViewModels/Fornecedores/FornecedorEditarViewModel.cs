b3c254d5395a36d02bade0d4356d727af6bd6921
willljdev <willljdev@gmail.com>
2022-09-07 13:50:30 -0300

    Criado FornecedorEditarViewModel

---
+++ Servico/ViewModels/Fornecedores/FornecedorEditarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servico.ViewModels.Fornecedores
+{
+    internal class FornecedorEditarViewModel : FornecedorCadastrarViewModel
+    {
+        public int Id { get; set; }
+    }
+}
