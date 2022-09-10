3f97ad7ef2f71306b7ebc9db5ad57b5ca196e6a0
GustavoPrim <gugahprm@gmail.com>
2022-09-09 21:16:55 -0300

    Feito ClienteMapeamento porém falta adicionar n BD

--- Repositorio/Mapeamentos/ClienteMapeamento.cs
+++ Repositorio/Mapeamentos/ClienteMapeamento.cs
@@ -12,15 +12,54 @@ namespace Repositorio.Mapeamentos
 
             builder.HasKey(x => x.Id);
 
+            builder.Property(x => x.Nome)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(100)
+                .IsRequired()
+                .HasColumnName("nome");
+
             builder.Property(x => x.Cpf)
                 .HasColumnType("VARCHAR")
+                .HasMaxLength(20)
                 .IsRequired()
                 .HasColumnName("cpf");
 
             builder.Property(x => x.Cnpj)
                 .HasColumnType("VARCHAR")
+                .HasMaxLength(14)
                 .IsRequired()
                 .HasColumnName("cnpj");
+
+            builder.Property(x => x.DataNascimento)
+                .HasColumnType("DATETIME2")
+                .IsRequired()
+                .HasColumnName("DataNascimento");
+
+            builder.Property(x => x.Endereco)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(200)
+                .IsRequired()
+                .HasColumnName("endereco");
+
+            builder.Property(x => x.Email)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(100)
+                .IsRequired()
+                .HasColumnName("email");
+
+            builder.Property(x => x.Telefone)
+                .HasColumnType("VARCHAR")
+
+                .HasMaxLength(100)
+                .HasColumnName("telefone");
+
+            builder.Property(x => x.Crea)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(200)
+                .IsRequired()
+                .HasColumnName("crea");
+
+            // adicionar pelo menos 2 no banco de dados
         }
     }
 }
