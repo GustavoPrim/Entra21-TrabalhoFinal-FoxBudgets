ebfc56d57e5bb2ddbf75c7156eacf0eb413f5a04
Lucas Perez Alves <perezcfnpbnu@gmail.com>
2022-09-11 22:38:55 -0300

    Iniciado a parte de Login -Foi realizado a primeira parte da tela de login com erros a ser corrigido pelo professor

---
+++ Aplicacao.Administrador/Controllers/Login/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Aplicacao.Administrador.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacao.Administrador.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            //if (Session["Erro"] != null)
+                //ViewBag.Erro = Session["Erro"].ToString();
+
+            return View();
+        }
+
+		[HttpPost]
+        public void ChecarLogin()
+		{
+            var usuario = new Usuarios();
+			//usuario.Email = Request["Email"];
+            //usuario.Senha = Request["PassWord"];
+
+            if (usuario.Login())
+            {
+                //Session["Autorizado"] = "OK";
+                //Session.Remove("Erro");
+                Response.Redirect("/Home/Index");
+            }
+			else
+			{
+                Response.Redirect("/Login/Index");
+                //Session["Erro"] = "Senha ou usuário invál";
+			}
+		}
+    }
+}
