c27e26c937a7e833c4034a153d9519b6b1ee4fe5
Lucas Perez <perezcfnpbnu@gmail.com>
2022-09-12 22:02:22 -0300

    Realizado alteracoes na tela de login

--- Aplicacao.Administrador/Controllers/Login/LoginController.cs
+++ Aplicacao.Administrador/Controllers/Login/LoginController.cs
@@ -7,30 +7,8 @@ namespace Aplicacao.Administrador.Controllers
     {
         public IActionResult Index()
         {
-            //if (Session["Erro"] != null)
-                //ViewBag.Erro = Session["Erro"].ToString();
-
             return View();
         }
 
-		[HttpPost]
-        public void ChecarLogin()
-		{
-            var usuario = new Usuarios();
-			//usuario.Email = Request["Email"];
-            //usuario.Senha = Request["PassWord"];
-
-            if (usuario.Login())
-            {
-                //Session["Autorizado"] = "OK";
-                //Session.Remove("Erro");
-                Response.Redirect("/Home/Index");
-            }
-			else
-			{
-                Response.Redirect("/Login/Index");
-                //Session["Erro"] = "Senha ou usuário invál";
-			}
-		}
     }
 }
