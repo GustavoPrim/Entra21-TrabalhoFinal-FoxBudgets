0b820fa7a98e952778c7a22381e7890f0e36b949
willljdev <willljdev@gmail.com>
2022-09-13 21:23:08 -0300

    Atualização da aplicação do fornecedor

--- Aplicacao.Fornecedor/Program.cs
+++ Aplicacao.Fornecedor/Program.cs
@@ -1,9 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Repositorio.BancoDados;
+using Repositorio.Repositorios;
+using Servico.MapeamentoEntidades;
+using Servico.Servicos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddDbContext<OrcamentoContexto>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+
+
+builder.Services.AddScoped<IFornecedorMapeamentoEntidade, FornecedorMapeamentoEntidade>();
+builder.Services.AddScoped<IFornecedorServico, FornecedorServico>();
+builder.Services.AddScoped<IFornecedorReposistorio, FornecedorRepositorio>();
+
 var app = builder.Build();
+
+
+
+using (var scopo = app.Services.CreateScope())
+{
+    var contexto = scopo.ServiceProvider
+        .GetService<OrcamentoContexto>();
+    contexto.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -13,6 +36,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -21,5 +46,13 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.UseEndpoints(endpoint =>
+{
+    endpoint.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=ListarFornecedor}/{id?}");
+});
+
 
 app.Run();
