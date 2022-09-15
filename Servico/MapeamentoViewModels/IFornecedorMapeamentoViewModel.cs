a70e9447dbd534fa246784e83cb7e61c44c4bf4e
Lucas Perez <perezcfnpbnu@gmail.com>
2022-09-14 22:11:27 -0300

    Corrigido erro cliente e administrador

--- Servico/MapeamentoViewModels/IFornecedorMapeamentoViewModel.cs
+++ Servico/MapeamentoViewModels/IFornecedorMapeamentoViewModel.cs
@@ -5,7 +5,6 @@ namespace Servico.MapeamentoViewModels
 {
     public interface IFornecedorMapeamentoViewModel
     {
-        public FornecedorEditarViewModel ConstruirCom(Fornecedor fornecedor);
-        }
+         FornecedorEditarViewModel ConstruirCom(Fornecedor fornecedor);
     }
 }
