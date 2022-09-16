9e23a32d2dfff3fed6d69dafcd5ed3fb6489db52
Lucas Perez <perezcfnpbnu@gmail.com>
2022-09-15 22:08:27 -0300

    Realizado o documento do controller, entidade/View -Foi feito a entidade -Foi feito o arquivo do controller -Foi feito o ViewModel

---
+++ Aplicacao.Fornecedor/Controllers/EstoqueController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacao.Fornecedor.Controllers
+{
+    public class EstoqueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
