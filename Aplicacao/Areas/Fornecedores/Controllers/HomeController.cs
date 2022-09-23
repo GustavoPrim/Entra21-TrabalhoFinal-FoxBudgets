938d83d86615fad855c0ba922cf82fd363625882
GustavoPrim <gugahprm@gmail.com>
2022-09-22 22:13:10 -0300

    Corrigido pequenas partes no css

--- Aplicacao/Areas/Fornecedores/Controllers/HomeController.cs
+++ Aplicacao/Areas/Fornecedores/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Aplicacao.Filtros;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Aplicacao.Areas.Fornecedores.Controllers
 {
     [Area("Fornecedores")]
     [Route("/fornecedor/")]
+    [UsuarioLogado]
     public class HomeController : Controller
     {
         [HttpGet]
