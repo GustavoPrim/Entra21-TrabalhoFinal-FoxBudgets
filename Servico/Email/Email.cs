7b3a2e06b2409b04568b537832cecf99a308aa01
Lucas Perez <perezcfnpbnu@gmail.com>
2022-09-28 21:59:59 -0300

    foi comecado a realizacao da verificacao email Adicionado Layout na home

---
+++ Servico/Email/Email.cs
@@ -0,0 +1,10 @@
+﻿namespace Servico.Email
+{
+    internal class Email : IEmail
+    {
+        public bool Enviar(string email, string assunto, string mensagem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
