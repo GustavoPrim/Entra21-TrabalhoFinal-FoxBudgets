b055cc0189af1510332708bca3a7fdbf5d42e819
Guilherme DMueller <guilhermemuellerdzesigaleski@gmail.com>
2022-09-30 20:20:52 -0300

    Index Deletado; Validação Nome do material deletado.

--- Servico/ViewModels/Materiais/MateriaisCadastrarViewModel.cs
+++ Servico/ViewModels/Materiais/MateriaisCadastrarViewModel.cs
@@ -6,8 +6,7 @@ namespace Servico.ViewModels.Materiais
     {
         [Display(Name = "Nome do produto")]
         [Required(ErrorMessage = "Informe nome do Material")]
-        [RegularExpression(@"^[a-zãçA-Z''-'\s]{1,100}$", ErrorMessage =
-            "Números e caracteres especiais não são permitidos no nome.")]
+        
         public string Nome { get; set; }
 
         [Display(Name = "Código Sku")]
