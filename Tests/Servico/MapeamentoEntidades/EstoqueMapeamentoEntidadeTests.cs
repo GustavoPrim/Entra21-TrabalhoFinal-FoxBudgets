307ecc9fcc4dabffea4ba9e8f887fc4078ea5105
Lucas Perez <perezcfnpbnu@gmail.com>
2022-09-29 21:53:23 -0300

    Comecado a realizacao dos Testes Foi realizado MapeamentoEntidades Administrador Foi realizado o MapeamentoEntidade Fornecedor Foi realizado o MapeamentoEntidade Cliente Comecado Estoque

---
+++ Tests/Servico/MapeamentoEntidades/EstoqueMapeamentoEntidadeTests.cs
@@ -0,0 +1,30 @@
+﻿using Servico.MapeamentoEntidades;
+using Servico.ViewModels.Estoque;
+using Xunit;
+
+namespace Tests.Servico.MapeamentoEntidades
+{
+    public class EstoqueMapeamentoEntidadeTests
+    {
+        private readonly IEstoqueMapeamentoEntidade _estoqueMapeamentoEntidade;
+
+        public EstoqueMapeamentoEntidadeTests()
+        {
+            _estoqueMapeamentoEntidade = new EstoqueMapeamentoEntidade();
+        }
+
+        [Fact]
+        public void Test_construirCom()
+        {
+            //Arrange
+            var estoque = new EstoqueCadastrarViewModel
+            {
+                Nome = "estocao",
+                Quantidade = 1,
+                Valor = 10
+            };
+            //Act
+            //Assertion
+        }
+    }
+}
