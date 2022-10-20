d7afa58d4f99931ba27dd3810a5de1b9f7d2115b
Lucas Perez <perezcfnpbnu@gmail.com>
2022-10-20 19:40:21 -0300

    Mudado de decimal para double no valor

--- Repositorio/Repositorios/OrcamentoRepositorio.cs
+++ Repositorio/Repositorios/OrcamentoRepositorio.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Repositorio.BancoDados;
 using Repositorio.Entidades;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repositorio.Repositorios
 {
--- Servico/ViewModels/Estoque/EstoqueItemIndexViewModel.cs
+++ Servico/ViewModels/Estoque/EstoqueItemIndexViewModel.cs
@@ -5,7 +5,7 @@ namespace Servico.ViewModels.Estoque
     public class EstoqueItemIndexViewModel
     {
         public int Quantidade { get; set; }
-        public decimal Valor { get; set; }
+        public double Valor { get; set; }
         public Material Material { get; set; }
         public int EstoqueMaterialId { get; set; }
 
