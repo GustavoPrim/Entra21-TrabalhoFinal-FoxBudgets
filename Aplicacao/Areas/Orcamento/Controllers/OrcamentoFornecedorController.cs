313994ef35b7c12f76ef9046c0851c0a50b9ddc7
PMSC <perezcfnpbnu@gmail.com>
2022-10-24 16:39:18 -0300

    Realizado novo metodo que faltava Criado controller Criado viewModel

---
+++ Aplicacao/Areas/Orcamento/Controllers/OrcamentoFornecedorController.cs
@@ -0,0 +1,15 @@
+﻿using Aplicacao.Filtros;
+using Aplicacao.Help;
+using Microsoft.AspNetCore.Mvc;
+using Servico.Servicos;
+
+namespace Aplicacao.Areas.Orcamento.Controllers
+{
+    [UsuarioLogado]
+    public class OrcamentoFornecedorController : Controller
+    {
+        private readonly IEstoqueServico _estoqueServico;
+        private readonly ISessao _sessao;
+        private readonly IMaterialService _materialService;
+    }
+}
