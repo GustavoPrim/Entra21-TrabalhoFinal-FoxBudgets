35d8821cae3b3321c8340aa6e90664e84f7a931e
PMSC <perezcfnpbnu@gmail.com>
2022-10-24 17:19:34 -0300

    Colocado um "s" nos fornecedores

--- Servico/ViewModels/Orcamentos/OrcamentoFornecedorViewModel.cs
+++ Servico/ViewModels/Orcamentos/OrcamentoFornecedorViewModel.cs
@@ -4,7 +4,7 @@ namespace Servico.ViewModels.Orcamentos
 {
     public class OrcamentoFornecedorViewModel
     {
-        public Fornecedor Fornecedore { get; set; }
+        public Fornecedor Fornecedores { get; set; }
         public string Item { get; set; }
         public double Valor { get; set; }
         public int Quantidade { get; set; }
